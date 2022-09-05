06acd2affbfc212b4840c1b04ba313af3a2a4b68
Ivan <ihk225@gmail.com>
2022-09-05 14:30:46 -0300

    All loops exercises fixes

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-ld = int(repr(number)[-1])
+ld = int(repr(number)[-1]) if number > 0 else (int(repr(number)[-1])) * -1
 if ld == 0:
     extra = " and is 0"
 elif ld != 0 and ld < 6:
