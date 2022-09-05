f326e7d5c1fe6b7018e363ffffca1d5a2fce7c99
Ivan <ihk225@gmail.com>
2022-09-05 15:18:35 -0300

    Loops fixes

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
-    res = a
-    for i in range(1, b):
-        res *= a
+    res = 1
+    while b != 0:
+        if b > 0:
+            res *= a
+            b -= 1
+        else:
+            res /= a
+            b += 1
     return res
+
