f6df9c22aab13e1a64772ad72386fe8e7174b015
Ivan <ihk225@gmail.com>
2022-09-05 17:02:53 -0300

    ex2_fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys as s
-    for i in range(len(s.argv)):
+    pl = "s" if len(s.argv) > 1 else ""
+    print("{} argument{}:")
+    for i in range(len(s.argv) + 1):
         print("{}: {}".format(i, s.argv[i]))
