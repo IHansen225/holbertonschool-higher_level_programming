d8e8338124572c2342cfd84ac2295c77842066df
Ivan <ihk225@gmail.com>
2022-09-05 17:10:12 -0300

    ex2_fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     if len(s.argv) == 1:
         print("0 arguments.")
     else:
-        pl = ":" if len(s.argv) == 1 else "s:"
+        pl = ":" if len(s.argv) == 2 else "s:"
         print("{} argument{}".format(len(s.argv) - 1, pl))
         for i in range(1, len(s.argv)):
             print("{}: {}".format(i, s.argv[i]))
