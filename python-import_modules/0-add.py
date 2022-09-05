ef96dab83b6b6ef3704bfd1dd046941788050f7c
Ivan <ihk225@gmail.com>
2022-09-05 16:18:50 -0300

    Ex0 fix commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from __add_0__ import add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+	from __add_0__ import add
+	a = 1
+	b = 2
+	print("{} + {} = {}".format(a, b, add(a, b)))
