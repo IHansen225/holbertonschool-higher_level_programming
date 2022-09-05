8a95de9ed30023cc4934de359a567b3329d5e3e2
Ivan <ihk225@gmail.com>
2022-09-05 14:38:40 -0300

    Loops fixes

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def pow(a, b):
     res = a
-    for i in range(0, abs(b-1)):
+    for i in range(1, b):
         res *= a
     return res
--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0, 10):
     for j in range(0, 10):
         if j % 10 > i:
             if ((i * 10) + j) != 89:
-                print("{:02d}".format(((i * 10) + j)), end = ", ")
+                print("{:02d}".format(((i * 10) + j)), end=", ")
             else:
-                print("{:02d}".format(((i * 10) + j)), end = "\n")
+                print("{:02d}".format(((i * 10) + j)), end="\n")
