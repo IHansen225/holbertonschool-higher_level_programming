63629b29147c5e03dd7745634f2583c9c086038b
Ivan <ihk225@gmail.com>
2022-09-05 18:05:01 -0300

    ex4_fpush

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
 	import hidden_4 as hd
-	print(dir(hd))
+	for i in range(len(dir(hd))):
+		if dir(hd)[i][1] != "_":
+			print("{}".format(dir(hd)[i]))
+
