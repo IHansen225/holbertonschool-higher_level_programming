000fd00cbe1ecd1c66146262c620e25b47011cb8
Ivan Hansen <ihk225@gmail.com>
2022-09-12 02:18:41 -0300

    Minor fixes

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
-    newlist = my_list
+    newlist = my_list.copy()
     newlist[idx] = element
     return newlist
--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print("{}".format("\n"))
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             s = " " if j != len(matrix[i]) - 1 else "\n"
