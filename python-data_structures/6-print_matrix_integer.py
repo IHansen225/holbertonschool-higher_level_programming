109aa5f1f16a2fbca5b679d83c628dd3879626c6
Ivan Hansen <ihk225@gmail.com>
2022-09-12 02:34:00 -0300

    6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(mat=[[]]):
     if len(mat[0]) == 0:
-        print("\n")
+        print("")
     for i in range(len(mat)):
         for j in range(len(mat[i])):
-            print("{:d}".format(mat[i][j]), end=" " if j + 1 != len(mat[i]) else "\n")
+            s = " " if j + 1 != len(mat[i]) else "\n"
+            print("{:d}".format(mat[i][j]), end=s)
