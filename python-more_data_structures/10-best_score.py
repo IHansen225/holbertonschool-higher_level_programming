882fe91c5d8f8410aa93762868bf154e8f69fa1a
Ivan Hansen <ihansen@pop-os.localdomain>
2022-09-13 15:18:42 -0300

    Last exercises

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,8 +1,4 @@
-#!/usr/bin/python3
 def best_score(a_dictionary):
-    sc = 0
-    key = ""
-    for i in a_dictionary:
-        sc = a_dictionary[i] if a_dictionary[i] > sc else sc
-        key = i if a_dictionary[i] > sc else key
-    return key
+    if not a_dictionary or a_dictionary is None:
+        return None
+    return max(a_dictionary, key=a_dictionary.get)
---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
+    rv = {
+        "M": 1000, "D": 500,
+        "C": 100, "L": 50,
+        "X": 10, "V": 5, "I": 1
+    }
+    val_ls = []
+    val = 0
+    for i in range(len(roman_string)):
+        for j in rv.keys():
+            if j == roman_string[i]:
+                val_ls.append(rv[j])
+    i = 0
+    while i < len(val_ls):
+        if i < len(val_ls) - 1 and val_ls[i] < val_ls[i + 1]:
+            val += val_ls[i + 1] - val_ls[i]
+            i += 2
+        else:
+            val += val_ls[i]
+            i += 1
+    return val
