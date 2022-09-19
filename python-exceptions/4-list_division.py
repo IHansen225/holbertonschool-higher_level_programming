61aab880141ccf9bad577dc58d716b3b8e4930cb
Ivan <ihk225@gmail.com>
2022-09-18 23:58:51 -0300

    Exceptions exercises

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -5,11 +5,14 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             nl.append(my_list_1[i] / my_list_2[i])
         except TypeError:
-            nl.append(0); print("wrong type")
+            nl.append(0)
+            print("wrong type")
         except ZeroDivisionError:
-            nl.append(0); print("division by 0")
+            nl.append(0)
+            print("division by 0")
         except:
-            nl.append(0); print("out of range")
+            nl.append(0)
+            print("out of range")
         finally:
             None
     return nl
