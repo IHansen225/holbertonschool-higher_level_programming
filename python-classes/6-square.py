3cf1c53f40287901ea96269af833bc013d93bed7
Ivan Hansen <ihk225@gmail.com>
2022-09-21 23:16:58 -0300

    last fixes

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -46,8 +46,8 @@ class Square:
         if self.__size == 0:
             print("")
         else:
-            mat = [[" " for i in range(self.__position[0])] for j in range(self.__size)]
-            for i in range(self.__size):
-                mat[i] = mat[i] + ["#" for i in range(self.__size)]
-            for i in range(self.__size):
-                print("".join(mat[i]))
+            if self.__position[1] > 0:
+                for i in range(self.__position[1]):
+                    print("")
+            for j in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
