9c5583c78fe876f93558178521257167b788386b
Ivan Hansen <ihk225@gmail.com>
2022-09-21 23:17:26 -0300

    last fixes

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -31,7 +31,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     @position.setter
     def position(self, value):
         if len(value) != 2 or type(value) != tuple:
