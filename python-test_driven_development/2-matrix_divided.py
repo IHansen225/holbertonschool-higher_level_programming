fc89fbffb8e2b248f635851f30a83d01c9a4df08
Ivan Hansen <ihk225@gmail.com>
2022-09-23 20:15:40 -0300

    Ex1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,6 +5,7 @@
 def matrix_divided(m, div):
     """ divides all elements from a matrix """
     rl = len(m[0])
+    err = "matrix must be a matrix (list of lists) of integers/floats"
     nmat = [[0 for i in range(rl)] for j in range(rl)]
     if div == 0:
         raise ZeroDivisionError("division by zero")
@@ -15,6 +16,6 @@ def matrix_divided(m, div):
             raise TypeError("Each row of the matrix must have the same size")
         for j in range(len(m[i])):
             if type(m[i][j]) is not int and type(m[i][j]) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            nmat[i][j] = float('%.2f'%(m[i][j] / div))
+                raise TypeError(err)
+            nmat[i][j] = float('%.2f'%(m[i][j]/div))
     return nmat
