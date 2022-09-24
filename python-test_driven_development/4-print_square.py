d0158633e65cbfa14e3b8713fecdcb0b80d8de3e
Ivan Hansen <ihk225@gmail.com>
2022-09-24 16:42:49 -0300

    Ex4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -3,12 +3,11 @@
 
 
 def print_square(size):
+	""" print square """
 	if type(size) is not int or type(size) is float and size < 0:
 		raise TypeError("size must be an integer")
 	elif size < 0:
 		raise ValueError("size must be >= 0")
-	if size == 0:
-		print("")
 	sq = [["#" for i in range(size)] for j in range(size)]
 	for i in range(size):
 		print("".join(sq[i]))
