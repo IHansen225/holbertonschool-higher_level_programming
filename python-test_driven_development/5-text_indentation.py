9f58a0f46e35f5048d62ef069104b321f9371a61
Ivan Hansen <ihk225@gmail.com>
2022-09-24 17:05:22 -0300

    Ex4

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -2,11 +2,11 @@
 """ text indentation function """
 
 def text_indentation(text):
-	""" print two new lines after .?: """
-	if type(text) is not str:
-		raise TypeError("text must be a string")
-	for i in text:
-		if i in {'.', '?', ':'}:
-			print("\n")
-		else:
-			print(i, end="")
+    """ print two new lines after .?: """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    for i in text:
+        if i in {'.', '?', ':'}:
+            print(f"{i}\n")
+        else:
+            print(i, end="")
