a229a4e81584d397294ff86b8ae6d6a395114359
Ivan Hansen <ihk225@gmail.com>
2022-09-25 19:57:55 -0300

    Ex7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -24,7 +24,6 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = height
-        self.print_symbol = "#"
 
     @property
     def width(self):
@@ -62,7 +61,7 @@ class Rectangle:
 
     def __str__(self) -> str:
         s = ""
-        ps = self.print_symbol
+        ps = Rectangle.print_symbol
         if self.__height == 0 or self.__width == 0:
             return ""
         r = [[ps for i in range(self.__width)] for j in range(self.__height)]
@@ -75,7 +74,7 @@ class Rectangle:
 
     def __print__(self) -> print:
         s = ""
-        ps = self.print_symbol
+        ps = Rectangle.print_symbol
         r = [[ps for i in range(self.__width)] for j in range(self.__height)]
         for i in range(len(r)):
             print("".join(r[i]))
