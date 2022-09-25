9b77a62f741437b006453bd84d7e3f24595b7c93
Ivan Hansen <ihk225@gmail.com>
2022-09-25 20:11:08 -0300

    Ex7

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -85,6 +85,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
@@ -92,5 +93,6 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         return rect_1 if rect_1.area() >= rect_2.area() else rect_2
 
+    @classmethod
     def square(cls, size=0):
         return cls(size, size)
