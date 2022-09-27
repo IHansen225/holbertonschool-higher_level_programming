b64105df651b53c8cfb408918e9501db1eade932
Ivan Hansen <ihk225@gmail.com>
2022-09-27 20:09:08 -0300

    Ex4

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" empty BaseGeometry class """
+
+
+class BaseGeometry():
+    """ BaseGeometry class """
+    def area(self):
+        raise Exception("area() is not implemented")
