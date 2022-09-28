5acf7a2e946c91f8e127bf41f441d312521e2f47
Ivan Hansen <ihk225@gmail.com>
2022-09-28 18:56:53 -0300

    IO project

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -7,6 +7,7 @@ write to a text file
 def write_file(filename="", text=""):
     """
     function to write to a text file
+    Return: number of characters added
     """
     with open(filename, 'a', encoding='utf-8') as f:
         f.write(text)
