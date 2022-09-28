28427c0dc1136a678a62b6da186f7e2220d44fa0
Ivan Hansen <ihk225@gmail.com>
2022-09-28 16:56:09 -0300

    IO project

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" write to a text file """
+
+
+def write_file(filename="", text=""):
+    """ function to write to a text file """
+    with open(filename, "wf") as f:
+        f.write(text)
+    return len(text)
