abbaeeb997a0cdee80be685e033792360c458e3d
Ivan Hansen <ihk225@gmail.com>
2022-09-28 15:53:37 -0300

    Ex11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" square class """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ square class inherited from Rectangle """
+
+    def __init__(self, size):
+        """ init function """
+        self.integer_validator("size", size)
+        __size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return super().area()
+
+    def __str__(self) -> str:
+        return f"[Square] {self.__size} / {self.__size}"
