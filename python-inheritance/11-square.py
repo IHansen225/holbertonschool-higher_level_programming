533ed62000da675f4c8aff6ecae04eab25bdce40
Ivan Hansen <ihk225@gmail.com>
2022-09-28 15:56:49 -0300

    Ex11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -9,8 +9,8 @@ class Square(Rectangle):
     def __init__(self, size):
         """ init function """
         self.integer_validator("size", size)
-        __size = size
         super().__init__(size, size)
+        __size = size
 
     def area(self):
         return super().area()
