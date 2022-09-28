9a363b628b68b6c97614c796192bf85b8656b2b7
Ivan Hansen <ihk225@gmail.com>
2022-09-28 20:13:47 -0300

    IO project

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
 """ add arguments to list in file """
-import sys, json
+import sys, json, os.path
 
+sjson = __import__('5-save_to_json_file').save_to_json_file
+ljson = __import__('6-load_from_json_file').load_from_json_file
 
-def save_to_json_file(my_obj, filename):
-    """ save JSON object to file """
-    with open(filename, 'w') as f:
-        jo = json.dumps(my_obj)
-        f.write(jo)
-
-def load_from_json_file(filename):
-    """ create JSON from file """
-    with open(filename, 'r') as f:
-        return json.load(f)
-
-def append_write(argv):
-	""" append arguments to write """
-	lf = load_from_json_file("add_item.json")
-	for i in range(len(argv)):
-		lf.append(argv[i])
-	save_to_json_file(lf, "add_item.json")
-
-append_write(sys.argv)
+fp = "add_item.json"
+ls = ljson(fp) if os.path.exists(fp) else []
+for i in sys.argv[1:]:
+	ls.append(i)
+sjson(ls, fp)
