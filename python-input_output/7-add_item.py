9d9b1d6d3ec551ce70eea427df45455eb467b132
Ivan Hansen <ihk225@gmail.com>
2022-09-28 20:19:00 -0300

    IO project

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ add arguments to list in file """
-import sys, json, os.path
+import sys
+import os.path
 
 sjson = __import__('5-save_to_json_file').save_to_json_file
 ljson = __import__('6-load_from_json_file').load_from_json_file
+
 
 fp = "add_item.json"
 ls = ljson(fp) if os.path.exists(fp) else []
