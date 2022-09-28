a40d93d2c4d556724cd9c952676f9d4ccad3a1d4
Ivan Hansen <ihk225@gmail.com>
2022-09-28 20:55:08 -0300

    permissions

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-""" add arguments to list in file """
+"""
+Task 7 module: Load, add, save
+"""
+import json
 import sys
 import os.path
-sjson = __import__('5-save_to_json_file').save_to_json_file
-ljson = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 args = sys.argv[1:]
 new_list = []
 
 if os.path.exists(filename):
-    new_list = ljson(filename)
+    new_list = load_from_json_file(filename)
 
 for item in args:
     new_list.append(item)
-ljson(new_list, filename)
+save_to_json_file(new_list, filename)
