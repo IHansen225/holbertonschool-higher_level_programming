996b8b261cd3308c99c86a51ea379afd80df35b8
Ivan Hansen <ihk225@gmail.com>
2022-09-28 19:47:29 -0300

    IO project

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,6 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """ create JSON from file """
-    with open(filename, 'a', encoding='utf-8') as f:
-        jo = json.dumps(f)
-        return jo
+    with open(filename, 'r') as f:
+        return json.load(f)
