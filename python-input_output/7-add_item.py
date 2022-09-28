a7168a411b014916dac4b4cb61f8964cccc29a27
Ivan Hansen <ihk225@gmail.com>
2022-09-28 20:54:39 -0300

    permissions

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,7 +5,6 @@ import os.path
 sjson = __import__('5-save_to_json_file').save_to_json_file
 ljson = __import__('6-load_from_json_file').load_from_json_file
 
-
 filename = "add_item.json"
 args = sys.argv[1:]
 new_list = []
