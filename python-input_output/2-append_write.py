da206b4843f9c45aadcce6e5105e802a0c66c60e
Ivan Hansen <ihk225@gmail.com>
2022-09-28 18:56:08 -0300

    IO project

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-""" write to a text file """
+"""
+write to a text file
+"""
 
 
 def write_file(filename="", text=""):
-    """ function to write to a text file """
+    """
+    function to write to a text file
+    """
     with open(filename, 'a', encoding='utf-8') as f:
         f.write(text)
