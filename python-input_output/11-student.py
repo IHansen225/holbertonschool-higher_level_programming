e652af999ae7eefa5e52425b3d96e6e2d5354564
Ivan Hansen <ihk225@gmail.com>
2022-09-28 22:55:26 -0300

    IO project

---
+++ python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" student class with function """
+
+
+class Student():
+    """ student class """
+
+    def __init__(self, first_name, last_name, age):
+        """ init function//constructor """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, at=None):
+        if at is None:
+            return self.__dict__.copy()
+        else:
+            dic = {}
+            for i in at:
+                for k, v in self.__dict__.items():
+                    if k == i:
+                        dic[k] = v
+            return dic
+
+    def reload_from_json(self, json):
+        for k, v in self.__dict__.items():
+            self.k = v
