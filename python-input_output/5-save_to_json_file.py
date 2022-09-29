97b5206d57010402b6a8c6ef7a13f0671506d755
Ivan Hansen <ihk225@gmail.com>
2022-09-28 21:01:14 -0300

    permissions

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,6 +5,8 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """ save JSON object to file """
+    if my_obj is None:
+        return
     with open(filename, 'a', encoding='utf-8') as f:
         jo = json.dumps(my_obj)
         f.write(jo)
