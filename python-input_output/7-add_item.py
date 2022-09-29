64927bc3f2fa6c62223c7cfdfbdec2fd125c4fca
Ivan Hansen <ihk225@gmail.com>
2022-09-28 23:15:59 -0300

    IO project

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,6 +8,6 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 fp = "add_item.json"
 ls = load_from_json_file(fp) if os.path.exists(fp) else []
 
-for i in sys.argv:
+for i in range(1, len(sys.argv)):
     ls.append(i)
 save_to_json_file(ls, fp)
