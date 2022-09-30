2b0be4d94138f4e33a38979364e3b9c01920fb8f
Ivan Hansen <ihk225@gmail.com>
2022-09-30 20:01:47 -0300

    Ex1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Project: "Almost a circle"
+File contents: Base class constructor and methods
+"""
+
+
+class Base():
+	"""
+	Base class
+
+	__nb_objects: generic ID for unidentified objects assigned when ID is None
+	id: unique identifier for objects
+	"""
+	__nb_objects = 0
+
+	def __init__(self, id=None):
+		""" init function // constructor """
+		self.__nb_objects += 1 if id is None else 0
+		self.id = self.__nb_objects if id is None else id
