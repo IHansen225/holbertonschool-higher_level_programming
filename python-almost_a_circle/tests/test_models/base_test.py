41fce396dd04e9008cbe186ff5b306c24b4e76ab
Ivan Hansen <ihk225@gmail.com>
2022-10-06 22:43:42 -0300

    First tests

---
+++ python-almost_a_circle/tests/test_models/base_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" unittest for Base class """
+
+
+import unittest
+from models.base import Base
+
+class BaseClassTest(unittest.TestCase):
+    
+    def test_autoID(self):
+        b = Base()
+        self.assert_equal(b.id, 1) 
+
+if __name__ == '__main__':
+    unittest.main()
