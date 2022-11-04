befe7f8d741229adb1111d63067ffce64855303b
Ivan Hansen <ihk225@gmail.com>
2022-11-04 17:14:08 -0300

    Ex9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,13 @@
 -- Create table if not exists and add values
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
-    name VARCHAR(255),
+    name VARCHAR(256),
     score INT
 );
-INSERT INTO second_table (id, name, score) VALUES (1, “John”, 10), (2, “Alex”, 3), (3, “Bob”, 14), (4, “George”, 8);
+INSERT INTO second_table
+    (id, name, score)
+VALUES
+    (1, “John”, 10),
+    (2, “Alex”, 3),
+    (3, “Bob”, 14),
+    (4, “George”, 8);
