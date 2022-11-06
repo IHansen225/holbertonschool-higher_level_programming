b57b08a9ede3fa34187403a7c32afd614dfd51d6
Ivan Hansen <ihk225@gmail.com>
2022-11-05 21:04:59 -0300

    EX3

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,2 @@
 -- Create a table with a non null column
-CREATE TABLE IF NOT EXISTS 'force_name' (
-    id INT,
-    name varchar(256) NOT NULL
-)
+CREATE TABLE IF NOT EXISTS force_name (id INT, name varchar(256) NOT NULL);
