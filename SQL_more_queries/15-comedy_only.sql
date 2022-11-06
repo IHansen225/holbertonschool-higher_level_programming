cb5e6a25810b39f92c18c7823c84edb14c127eb0
Ivan Hansen <ihk225@gmail.com>
2022-11-05 22:38:11 -0300

    EX15

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,2 @@
+-- Multiple join with constraints
+SELECT tv_shows.title FROM tv_show_genres JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_genres.name = "Comedy" ORDER BY tv_shows.title ASC;
