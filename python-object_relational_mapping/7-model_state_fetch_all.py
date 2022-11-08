dc32f6012b68708bb025702e4283095c7353e6c2
Ivan Hansen <ihk225@gmail.com>
2022-11-08 19:31:55 -0300

    ORM base model

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""
+    Approach to SQLAlchemy ORM
+    First query exercise
+"""
---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    Base model class for ORM
+    instance methods
+"""
+import sys
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+        Base constructor for query methods
+    """
+    __tablename__ = 'states'
+
+    id = sa.Column(sa.Integer, primary_key=True, nullable=False)
+    name = sa.Column(sa.String(128), nullable=True)
+
+
+if __name__ == '__main__':
+    pass
