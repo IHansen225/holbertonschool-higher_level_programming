b1924f6725a158b119766cd80be6a9392b23dcf7
Ivan Hansen <ihk225@gmail.com>
2022-11-08 20:32:35 -0300

    Ex10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Approach to SQLAlchemy ORM
+    First query exercise
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine as eng
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = eng(f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}")
+
+    Session = sessionmaker(engine)
+    session = Session()
+
+    states_query = session.query(State).filter(
+        State.name == f"{argv[4]}"
+    )
+    for i in states_query:
+        print(f"{i.id}: {i.name}")
