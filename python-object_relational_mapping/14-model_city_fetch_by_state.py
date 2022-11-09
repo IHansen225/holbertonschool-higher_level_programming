c08f7db46761918bcb5fee12280121e4255542df
Ivan Hansen <ihk225@gmail.com>
2022-11-09 19:39:51 -0300

    Ex14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -4,21 +4,18 @@
     First query exercise
 """
 from sys import argv
+from model_city import City, Base
 from model_state import Base, State
-from model_city import City
 from sqlalchemy import create_engine as eng
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy import update
 
 if __name__ == "__main__":
     engine = eng(f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}")
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(engine)
     session = Session()
 
-    states_query = session.query(City, State).filter(
-        City.state_id == State.id
-    ).order_by(City.id)
-    for state, city in states_query:
-        print(f"{city.name}: ({state.id}) {state.name}")
+    states_query = session.query(City, State).filter(State.id == City.state_id).all()
+    for city, state in states_query:
+        print(f"{state.name}: ({city.id}) {state.name}")
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -3,16 +3,18 @@
     Base model class for ORM
     instance methods
 """
-import sqlalchemy as sa
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+
 
 Base = declarative_base()
 
+
 class City(Base):
     """
-        This class represents a city in the database
+        State class
     """
-    __tablename__ = "cities"
-    id = sa.Column(sa.Integer, primary_key=True)
-    name = sa.Column(sa.String(128), nullable=False)
-    state_id = sa.Column(sa.Integer, ForeignKey("states.id"), nullable=False)
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('State.id'), nullable=False)
