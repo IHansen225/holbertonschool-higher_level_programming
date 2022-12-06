878cf493dd871c1a61104504557fbf6bc763a3c1
Ivan Hansen <ihk225@gmail.com>
2022-12-06 17:19:21 -0300

    Ex4

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 console.log(`${process.argv[2] !== undefined ? process.argv[2] : undefined}\
-is ${process.argv[3] !== undefined ? process.argv[3] : undefined}`);
+ is ${process.argv[3] !== undefined ? process.argv[3] : undefined}`);
