06bef22c755cc65288b9d19d00d94f7c16f2da04
Ivan Hansen <ihk225@gmail.com>
2022-12-06 19:06:10 -0300

    Multiples Exs commit

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,9 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     const isValid = ((w < 0 || w !== undefined) && (h < 0 || h !== undefined));
-    this.width = isValid ? w : undefined;
-    this.height = isValid ? h : undefined;
+    if (isValid) {
+      this.width = isValid ? w : undefined;
+      this.height = isValid ? h : undefined;
+    }
   }
 };
