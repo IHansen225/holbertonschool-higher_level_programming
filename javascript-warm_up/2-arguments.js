9e43671955cd59bab776fe31e499e0c7fb17cfba
Ivan Hansen <ihk225@gmail.com>
2022-12-06 17:13:35 -0300

    Ex2 correction

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.length;
 console.log(`${args === 1
-    ? 'No a'
-    : 'A'}rgument${args === 2
-        ? ' found'
-        : (args > 2
-            ? 's found'
-            : '')}`);
+  ? 'No argument'
+  : (args === 2
+    ? 'Argument found'
+    : 'Arguments found')}`);
