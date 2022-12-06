1847629fb62600a1d5f0792b29dbdb0974bd529f
Ivan Hansen <ihk225@gmail.com>
2022-12-06 19:28:24 -0300

    Ex4 commit

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    const invalid = ((w < 0 || w === undefined) || (h < 0 || h === undefined));
+    if (!invalid) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  double () { this.width *= 2; this.height *= 2; }
+
+  rotate () { this.width = [this.height, this.height = this.width][0]; }
+};
