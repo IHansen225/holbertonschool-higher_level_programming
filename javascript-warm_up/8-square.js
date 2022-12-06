d85d1599afc3b9296e150e8dd59671573c3a3254
Ivan Hansen <ihk225@gmail.com>
2022-12-06 17:49:45 -0300

    Multiple corrections

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (!(isNaN(parseInt(process.argv[2], 10)))) {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
-    console.log('x'.repeat(parseInt(process.argv[2])));
+    console.log('X'.repeat(parseInt(process.argv[2])));
   }
 } else {
   console.log('Missing size');
