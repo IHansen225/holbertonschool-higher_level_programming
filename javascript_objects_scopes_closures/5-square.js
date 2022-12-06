4e2dd0f86e86104e292f1117f74fbcd4b310c51c
Ivan Hansen <ihk225@gmail.com>
2022-12-06 19:33:09 -0300

    Ex5 commit

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,26 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    const invalid = ((w < 0 || w === undefined) || (h < 0 || h === undefined));
+    if (!invalid) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  double () { this.width *= 2; this.height *= 2; }
+
+  rotate () { this.width = [this.height, this.height = this.width][0]; }
+}
+
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size);
   }
-}
+};
