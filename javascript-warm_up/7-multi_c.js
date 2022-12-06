d63f0af791a4aea5837fad2817cc1213187aa7e6
Ivan Hansen <ihk225@gmail.com>
2022-12-06 17:37:13 -0300

    Ex7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (!(isNaN(parseInt(process.argv[1], 10)))) {
-  for (let i = 0; i < parseInt(process.argv[1], 10); i++) {
+if (!(isNaN(parseInt(process.argv[2], 10)))) {
+  for (let i = 0; i < parseInt(process.argv[2], 10); i++) {
     console.log('C is fun');
   }
 } else {
