2db13b692845520466ff1cf9590ab864d3c4975f
Ivan Hansen <ihk225@gmail.com>
2022-12-06 19:33:06 -0300

    Test commit

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 let args = process.argv.length;
-console.log(`${args === 0 ? 'No a' : 'a'}rgument${args === 1 ? ' found' : (args > 1 ? 's found' : '')}`);
+console.log(`${args === 1 ? 'No a' : 'a'}rgument${args === 2 ? ' found' : (args > 2 ? 's found' : '')}`);
