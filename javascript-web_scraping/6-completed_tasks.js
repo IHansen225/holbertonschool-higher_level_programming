812e38be634b5e949f178a6b9aef6ca422408ea0
Ivan Hansen <ihk225@gmail.com>
2022-12-14 00:21:44 -0300

    Semistandard corrections

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -5,13 +5,13 @@ request.get(`${process.argv[2]}`, function (err, res, body) {
   if (err) {
     console.error(err);
   }
-  res_dict = {};
-  for (task of JSON.parse(body)) {
-    if (!(task.userId in res_dict) && task.completed) {
-      res_dict[task.userId] = 1;
-    } else if (!(task.userid in res_dict) && task.completed) {
-      res_dict[task.userId]++;
+  const dic = {};
+  for (const task of JSON.parse(body)) {
+    if (!(task.userId in dic) && task.completed) {
+      dic[task.userId] = 1;
+    } else if (!(task.userid in dic) && task.completed) {
+      dic[task.userId]++;
     }
   }
-  console.log(res_dict);
+  console.log(dic);
 });
