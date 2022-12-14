5ba0911d277fe24ad1b64ff9e3784e82af1048b8
Ivan Hansen <ihk225@gmail.com>
2022-12-14 00:07:18 -0300

    Semistandard corrections

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const fs = require('fs');
 
-request.get(`${process.argv[2]}`, function(err, res, body) {
+request.get(`${process.argv[2]}`, function (err, res, body) {
   fs.writeFile(`${process.argv[3]}`, body, 'utf8', (err) => {
     if (err) {
       console.error(err);
--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
 
-request.get(`${process.argv[2]}`, function(err, res, body) {
+request.get(`${process.argv[2]}`, function (err, res, body) {
   res_dict = {};
   for (task of JSON.parse(body)) {
     if (!(task.userId in res_dict) && task.completed) {
-      res_dict[task.userId] = 1
+      res_dict[task.userId] = 1;
     } else if (!(task.userid in res_dict) && task.completed) {
       res_dict[task.userId]++;
     }
   }
-  console.log(res_dict)
+  console.log(res_dict);
 });
